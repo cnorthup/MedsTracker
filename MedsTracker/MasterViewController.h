e2775e51e7f20e0caeaf11c8e4abc40d13759248
Charles Northup <cjnorth40@gmail.com>
2015-05-12 12:52:26 -0500

    Initial Commit

---
+++ MedsTracker/MasterViewController.h
@@ -0,0 +1,19 @@
+//
+//  MasterViewController.h
+//  MedsTracker
+//
+//  Created by Charles Northup on 5/12/15.
+//  Copyright (c) 2015 RogueNotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ MedsTracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MedsTracker
+//
+//  Created by Charles Northup on 5/12/15.
+//  Copyright (c) 2015 RogueNotion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
